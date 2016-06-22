ab94ebe30e34faef1396a601183abd27dfaf108b
Jirong Hu <jirong_hu@otpp.com>
2016-06-22 11:16:15 -0400

    Nexus upload file w/o path works

--- C#/DevOps/Nexus/NexusAPI.cs
+++ C#/DevOps/Nexus/NexusAPI.cs
@@ -40,11 +40,15 @@ class NexusAPI
         
     }
 
-    public void uploadArtifact()
+    /// <summary>
+    /// Works
+    /// </summary>
+    public void uploadArtifact() 
     {
         RestRequest request = new RestRequest("artifact/maven/content", Method.POST);
-        request.RequestFormat = RestSharp.DataFormat.Json;
-        request.AddHeader("Content-Type", "x-www-form-urlencoded");
+        // Below two lines are optional
+        //request.RequestFormat = RestSharp.DataFormat.Json;
+        //request.AddHeader("Content-Type", "x-www-form-urlencoded");
         request.AddParameter("r", "releases");
         request.AddParameter("hasPom", "false");
         request.AddParameter("p", "zip");
@@ -52,13 +56,89 @@ class NexusAPI
         request.AddParameter("g", "otpp.devops");        
         request.AddParameter("a", "githubSuspsendInactiveADAccounts");
         request.AddParameter("v", "1.0.1");
-        request.AddParameter("file", "@C:\\Temp\\Nexus\\githubSuspsendInactiveADAccounts-1.0.1.zip");
-        request.AddFile("githubSuspsendInactiveADAccounts-1.0.1.zip", @"C:\\Temp\\Nexus\\githubSuspsendInactiveADAccounts-1.0.1.zip"); //Completed but not uploaded
+        request.AddParameter("file", "githubSuspsendInactiveADAccounts-1.0.1.zip");
+        request.AddFile("githubSuspsendInactiveADAccounts-1.0.1.zip", "githubSuspsendInactiveADAccounts-1.0.1.zip");
         
-                    
         IRestResponse response = this.restSharpAPI.Execute(this.client, request);
         Console.WriteLine("Response received: {0}", response.ResponseStatus);        
 
+    }
+
+    /// <summary>
+    /// Additional path to the file
+    /// </summary>
+    public void uploadArtifact1()
+    {
+        RestRequest request = new RestRequest("artifact/maven/content", Method.POST);
+        // Below two lines are optional
+        //request.RequestFormat = RestSharp.DataFormat.Json;
+        //request.AddHeader("Content-Type", "x-www-form-urlencoded");
+        request.AddParameter("r", "releases");
+        request.AddParameter("hasPom", "false");
+        request.AddParameter("p", "zip");
+        request.AddParameter("e", "zip");
+        request.AddParameter("g", "otpp.devops");
+        request.AddParameter("a", "githubSuspsendInactiveADAccounts");
+        request.AddParameter("v", "1.0.1");
+        request.AddParameter("file", "C:/Temp/Nexus/githubSuspsendInactiveADAccounts-1.0.1.zip");
+        request.AddFile("githubSuspsendInactiveADAccounts-1.0.1.zip", "C:/Temp/Nexus/githubSuspsendInactiveADAccounts-1.0.1.zip");
+        
+        IRestResponse response = this.restSharpAPI.Execute(this.client, request);
+        Console.WriteLine("Response received: {0}", response.ResponseStatus);
+
+    }
+
+    /// <summary>
+    /// https://blog.tyrsius.com/restsharp-file-upload/
+    /// </summary>
+    public void uploadArtifact2() // can't close stream until all bytes are written
+    {
+        RestRequest request = new RestRequest("artifact/maven/content/", Method.POST);
+        request.RequestFormat = RestSharp.DataFormat.Json;
+        request.AlwaysMultipartFormData = true;
+        request.AddHeader("Content-Type", "x-www-form-urlencoded");
+        request.AddParameter("r", "releases");
+        request.AddParameter("hasPom", "false");
+        request.AddParameter("p", "zip");
+        request.AddParameter("e", "zip");
+        request.AddParameter("g", "otpp.devops");
+        request.AddParameter("a", "githubSuspsendInactiveADAccounts");
+        request.AddParameter("v", "1.0.1");
+        request.AddParameter("file", "githubSuspsendInactiveADAccounts-1.0.1.zip");    
+        
+        string filename = "githubSuspsendInactiveADAccounts-1.0.1.zip";
+        FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+        request.AddFile("file", fs.CopyTo, filename);
+        fs.Close();
+
+        IRestResponse response = this.restSharpAPI.Execute(this.client, request);
+        Console.WriteLine("Response received: {0}", response.ResponseStatus);
+        
+    }
+
+    /// <summary>
+    /// http://drupal.stackexchange.com/questions/101872/attach-file-to-post-using-c-restsharp-and-drupal-services/143727#143727?newreg=0d4ecba4b7234432831af804a232fdb9
+    /// </summary>
+    public void uploadArtifact3()  //Caused by: java.lang.IllegalStateException: Form too large 832324>200000
+    {
+        RestRequest request = new RestRequest("artifact/maven/content/", Method.POST);
+        request.RequestFormat = RestSharp.DataFormat.Json;        
+        request.AddParameter("r", "releases");
+        request.AddParameter("hasPom", "false");
+        request.AddParameter("p", "zip");
+        request.AddParameter("e", "zip");
+        request.AddParameter("g", "otpp.devops");
+        request.AddParameter("a", "githubSuspsendInactiveADAccounts");
+        request.AddParameter("v", "1.0.1");
+        //request.AddParameter("file", "@C:\\Temp\\Nexus\\githubSuspsendInactiveADAccounts-1.0.1.zip");
+
+        string filename = "githubSuspsendInactiveADAccounts-1.0.1.zip";
+        string filevalue = System.Convert.ToBase64String(RestSharpAPI.file_get_byte_contents(filename));
+        request.AddParameter("file", filevalue);
+        request.AddParameter("filename", filename);
+
+        IRestResponse response = client.Execute<DFile>(request);
+        Console.WriteLine("Response received: {0}", response.ResponseStatus);
     }
 
     static void Main(string[] args)
@@ -94,7 +174,10 @@ class NexusAPI
                 Console.WriteLine("Repo info: {0}", repo.ToString());
             }
 
-            nexusAPI.uploadArtifact();
+            //nexusAPI.uploadArtifact();
+            nexusAPI.uploadArtifact1();
+            //nexusAPI.uploadArtifact2();
+            //nexusAPI.uploadArtifact3();
 
             Console.ReadKey(true);
         }
