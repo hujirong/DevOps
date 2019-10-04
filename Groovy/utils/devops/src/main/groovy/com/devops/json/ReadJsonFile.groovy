34df1749c17f6970ba56579293f6f4a6c5a565d0
Jirong Hu <jirong.hu@gmail.com>
2019-10-04 11:19:47 -0400

    test JIRA

--- Groovy/utils/devops/src/main/groovy/com/devops/json/ReadJsonFile.groovy
+++ Groovy/utils/devops/src/main/groovy/com/devops/json/ReadJsonFile.groovy
@@ -3,6 +3,6 @@ package com.devops.json
 import groovy.json.JsonSlurper
  
 def jsonSlurper = new JsonSlurper()
-//data = jsonSlurper.parse(new File("payload.json")) 
-//println(data)
+data = jsonSlurper.parse(new File("payload.json")) 
+println(data)
 
